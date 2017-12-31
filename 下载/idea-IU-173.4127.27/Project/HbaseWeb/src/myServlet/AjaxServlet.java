1be6f2cbec76fdfe056f4a0275fccb089c4357e4
Jane11111 <791057615@qq.com>
2017-12-31 18:46:14 +0800

    电影信息部分完成

--- 下载/idea-IU-173.4127.27/Project/HbaseWeb/src/myServlet/AjaxServlet.java
+++ 下载/idea-IU-173.4127.27/Project/HbaseWeb/src/myServlet/AjaxServlet.java
@@ -6,14 +6,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "TestServlet")
-public class TestServlet extends HttpServlet {
+@WebServlet(name = "AjaxServlet",urlPatterns = {"/myServlet/AjaxServlet"})
+public class AjaxServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String m_id=request.getParameter("m_id");
+        String attr=request.getParameter("attr");
+        String value=request.getParameter("value");
+        System.out.println("我得到了值"+m_id+" "+attr+" "+value+"");
+
+        PrintWriter out=response.getWriter();
+        out.println("成功");
+        out.flush();
+        out.close();
+
+
 
     }
 }
